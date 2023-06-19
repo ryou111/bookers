65a2f4b7e7787b499e6b22d082fb4abf764bbd03
EC2 Default User <ec2-user@ip-172-31-11-127.ap-northeast-1.compute.internal>
2023-06-19 05:23:05 +0000

    [finish]課題2

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :books, :except => :edit
-  get '/top' => 'homes#top'
+  get '/' => 'homes#top'
   get 'books/:id/edit' => 'books#edit', as: 'edit_book'
   patch 'books/:book' => 'books#update', as: 'update_book'
   delete 'books/:id' => 'books#destroy', as: 'destroy_book'
